f247764ec26f608ab00562858ccf6fcde1d46708
Nirbhay Gupta <guptanirbhay93@gmail.com>
2023-01-27 21:26:52 +0530

    [version] upgrade

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='rbmq-client',
     packages=['rbmq_client'],
-    version='1.0.0',
+    version='1.0.1',
     author="Develper Junio",
     author_email='developer@junio.in',
     classifiers=[
