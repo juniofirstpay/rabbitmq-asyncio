8a17b465af8dbab05ae439ba74cf6f7f16d83828
Developer Junio <developer@junio.in>
2023-03-17 21:42:05 +0530

    [build] version upgrade

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='rbmq-client',
     packages=['rbmq_client', 'rbmq_aio_client'],
-    version='2.2.17',
+    version='2.2.18',
     author="Develper Junio",
     author_email='developer@junio.in',
     classifiers=[
