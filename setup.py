2192f0be44c16a0fed6a67e3a9ba809731bc56c6
Anikait <anikait.makkar@outlook.com>
2023-09-28 17:14:59 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='rbmq-client',
     packages=['rbmq_client', 'rbmq_aio_client'],
-    version='1.0.0-canary',
+    version='0.2.0',
     author="Develper Junio",
     author_email='developer@junio.in',
     classifiers=[
